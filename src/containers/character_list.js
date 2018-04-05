6d2b36a088268587c9366f750e0b6422e5b45d8a
Jonathan Torres <Jonathan.torres25@live.com>
2018-04-04 23:01:56 -0400

    fix ajax

--- src/containers/character_list.js
+++ src/containers/character_list.js
@@ -14,7 +14,6 @@ class CharacterList extends Component {
   onComicClick(e){
     const timeStamp = Date.now();
     const comicUrl = e.target.attributes.getNamedItem('comic-data').value;
-    alert('hi')
     this.props.fetchComic(comicUrl, timeStamp);
   }
 
